634efed33f7ee8ba7a10ebe919bda1b540a45fc7
jeennny <31215594+jeennny@users.noreply.github.com>
2017-11-10 10:22:35 -0600

    commit6

---
+++ proyecto2/proyecto7/js/app.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+   $('.parallax').parallax();
+   $('.carousel.carousel-slider').carousel({fullWidth: true});
+   $(".button-collapse").sideNav();    
+   Materialize.updateTextFields();
+   $('select').material_select();
+   
+ });
+
+
+      
