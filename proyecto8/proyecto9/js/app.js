393315fda26e1d2aa7dfa0e2155ff8a63fbc3948
jeennny <31215594+jeennny@users.noreply.github.com>
2017-11-11 06:26:20 -0600

    commit 8

---
+++ proyecto8/proyecto9/js/app.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+   $('.parallax').parallax();
+   $('.carousel.carousel-slider').carousel({fullWidth: true});
+   $(".button-collapse").sideNav();    
+   Materialize.updateTextFields();
+   $('select').material_select();
+    $('.carousel').carousel();
+     $('.carousel.carousel-slider').carousel({fullWidth: true});
+ });
+
+
+      
